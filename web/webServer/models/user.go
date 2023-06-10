a8587a3db3dd01f54083d3ea387f528419971693
18002818781 <137194818@qq.com>
2023-06-10 21:17:12 +0800

    用户

--- web/webServer/models/user.go
+++ web/webServer/models/user.go
@@ -20,76 +20,187 @@ type UserClaim struct { // 登录验证
 
 // 收藏的笔记
 type Collects struct {
-	Cover     string `json:"cover"`
-	LikedNum  int64  `json:"likedNum"` // 点赞数
-	NoteID    int64  `json:"noteId"`   // 笔记编号
-	Title     string `json:"title"`
-	CreatorID int64  `json:"creatorID"`
-	Portrait  int64  `json:"portrait"`
+	Cover       string `json:"cover"`
+	LikedNum    int64  `json:"likedNum"` // 点赞数
+	NoteID      int64  `json:"noteId"`   // 笔记编号
+	Title       string `json:"title"`
+	CreatorID   int64  `json:"creatorID"`
+	CreatorName string `json:"creatorName"` // 作者姓名
+	Portrait    string `json:"portrait"`
 }
 
 // 点赞的笔记
 type Likes struct {
-	Cover     string `json:"cover"`
-	LikedNum  int64  `json:"likedNum"` // 点赞数
-	NoteID    int64  `json:"noteId"`   // 笔记编号
-	Title     string `json:"title"`
-	CreatorID int64  `json:"creatorID"`
-	Portrait  int64  `json:"portrait"`
+	Cover       string `json:"cover"`
+	LikedNum    int64  `json:"likedNum"` // 点赞数
+	NoteID      int64  `json:"noteId"`   // 笔记编号
+	Title       string `json:"title"`
+	CreatorID   int64  `json:"creatorID"`
+	CreatorName string `json:"creatorName"` // 作者姓名
+	Portrait    string `json:"portrait"`
 }
 
 // 发布的笔记
 type Notes struct {
-	Cover     string `json:"cover"`
-	LikedNum  int64  `json:"likedNum"` // 点赞数
-	NoteID    int64  `json:"noteId"`   // 笔记编号
-	Title     string `json:"title"`
-	CreatorID int64  `json:"creatorID"`
-	Portrait  int64  `json:"portrait"`
+	Cover       string `json:"cover"`
+	LikedNum    int64  `json:"likedNum"` // 点赞数
+	NoteID      int64  `json:"noteId"`   // 笔记编号
+	Title       string `json:"title"`
+	CreatorID   int64  `json:"creatorID"`
+	CreatorName string `json:"creatorName"` // 作者姓名
+	Portrait    string `json:"portrait"`
 }
 
 // 用户基本信息
 type UserInfo struct {
-	userID       int       `JSON:"UserID"`
-	userName     string    `JSON:"UserName"`
-	password     string    `JSON:"Password"`
-	gender       string    `JSON:"Gender"`
-	portrait     string    `JSON:"Portrait"`
-	introduction string    `JSON:"UserName"`
-	birthday     time.Time `JSON:"Birthday"`
-	registTime   time.Time `JSON:"RegistTime"`
-	fansNum      int       `JSON:"FansNum"`      // 粉丝数
-	noteNum      int       `JSON:"NoteNum"`      // 笔记数
-	collectNum   int       `JSON:"CollectNum"`   //收藏数
-	followNum    int       `JSON:"FollowNum"`    //关注数
-	collectedNum int       `JSON:"CollectedNum"` // 被收藏数量
-	likedNum     int       `JSON:"LikedNum"`     // 被点赞数量
-	phoneNumber  string    `JSON:"PhoneNumber"`
-	mail         string    `JSON:"Mail"`
+	Birthday     string `json:"birthday"`
+	CollectedNum int64  `json:"collectedNum"` // 被收藏的笔记数
+	CollectNum   int64  `json:"collectNum"`   // 收藏数
+	FansNum      int64  `json:"fansNum"`
+	FollowNum    int64  `json:"followNum"`    // 关注数
+	Gender       string `json:"gender "`      // 性别
+	Introduction string `json:"introduction"` // 简介
+	LikedNum     int64  `json:"likedNum"`     // 被点赞数量
+	Mail         string `json:"mail"`
+	NoteNum      int64  `json:"noteNum "`
+	Password     string `json:"password"`
+	PhoneNumber  string `json:"phoneNumber"`
+	Portrait     string `json:"portrait"` // 头像
+	RegistTime   string `json:"registTime"`
+	UserID       int64  `json:"userAccount"`
+	UserName     string `json:"userName "`
 }
 
-func UserInfoDB(id int) UserInfo { // 从数据库获得用户信息
-	sqlStr := `select *
-	from userinfo
-	where userAccount = '"+id+"'`
-	rows, err := db.Query(sqlStr)
+func UserInfoDB(id int) (uui []UserInfo) { // 从数据库获得用户信息
+	// var ui UserInfo
+	// sqlStr := `select userAccount, userName, gender, portrait, introduction, birthday, registTime, fansNum, noteNum, collectNum, followNum, collectedNum, likedNum, phoneNumber, mail, password
+	// from userinfo
+	// where userAccount = ?`
+	// err := db.QueryRow(sqlStr, id).Scan(&ui.userID, &ui.userName, &ui.gender, &ui.portrait, &ui.introduction, &ui.birthday, &ui.registTime, &ui.fansNum, &ui.noteNum, &ui.collectNum, &ui.followNum, &ui.collectedNum, &ui.likedNum, &ui.phoneNumber, &ui.mail, &ui.password)
+	// if err != nil {
+	// 	fmt.Printf("query failed, err:%v\n", err)
+	// 	return ui
+	// }
+	// return ui
 	var ui UserInfo
-	if err != nil {
-		fmt.Printf("query failed, err:%v\n", err)
-		return ui
-	}
-	// 关闭rows释放持有的数据库链接
-	defer rows.Close()
+	ui.UserID = 10001
+	ui.UserName = "zahgsad"
+	ui.Gender = "sha"
+	ui.Portrait = "/"
+	ui.Introduction = "jntm"
+	ui.Birthday = time.Now().Format("2006-01-02 15:04:05")
+	ui.RegistTime = time.Now().Format("2006-01-02 15:04:05")
+	ui.FansNum = 666
+	ui.NoteNum = 777
+	ui.CollectNum = 888
+	ui.FollowNum = 999
+	ui.CollectedNum = 1010
+	ui.LikedNum = 115
+	ui.PhoneNumber = "1233663"
+	ui.Mail = "1235"
+	ui.Password = "123456"
+	uui = append(uui, ui)
+	return uui
+}
 
-	// 循环读取结果集中的数据
-	for rows.Next() {
-		err := rows.Scan(&ui.userID, &ui.userName, &ui.gender, &ui.portrait, &ui.introduction, &ui.birthday, &ui.registTime, &ui.fansNum, &ui.noteNum, &ui.collectNum, &ui.followNum, &ui.collectedNum, &ui.likedNum, &ui.phoneNumber, &ui.mail, &ui.password)
-		if err != nil {
-			fmt.Printf("scan failed, err:%v\n", err)
-			return ui
-		}
-	}
-	return ui
+func NoteInfoDB(id int) (notes []Notes) { // 从数据库获得用户信息
+	// sqlStr := `select n.noteId, n.title, n.cover,n.creatorID,n.likeNum, u.portrait,n.creatorName
+	// from noteInfo n,userInfo u
+	// where n.creatorID = ? and u.userAccount = ?`
+	// rows, err := db.Query(sqlStr, id, id)
+	// if err != nil {
+	// 	fmt.Printf("query failed, err:%v\n", err)
+	// 	return
+	// }
+	// defer rows.Close()
+
+	// for rows.Next() {
+	// 	var nt Notes
+	// 	err := rows.Scan(&nt.NoteID, &nt.Title, &nt.Cover, &nt.CreatorID, &nt.LikedNum, &nt.Portrait, &nt.CreatorName)
+	// 	if err != nil {
+	// 		fmt.Printf("scan failed, err:%v\n", err)
+	// 		return
+	// 	}
+	// 	notes = append(notes, nt)
+	// }
+	// return
+
+	var nt Notes
+	nt.NoteID = 10001
+	nt.Title = "l就别"
+	nt.Cover = "站发给"
+	nt.CreatorID = 10001
+	nt.LikedNum = 500
+	nt.Portrait = "/"
+	nt.CreatorName = "张菲"
+	notes = append(notes, nt)
+	return
+}
+
+func CollectInfoDB(id int) (notes []Collects) { // 从数据库获得用户信息
+	// sqlStr := `select n.noteId, n.title, n.cover,n.creatorAccount,n.likeNum, u.portrait
+	// from noteInfo n,userInfo u
+	// where n.creatorAccount = '"+id+"' and u.userAccount = '"+id+"'`
+	// rows, err := db.Query(sqlStr)
+	// if err != nil {
+	// 	fmt.Printf("query failed, err:%v\n", err)
+	// 	return
+	// }
+	// defer rows.Close()
+
+	// for rows.Next() {
+	// 	var nt Collects
+	// 	err := rows.Scan(&nt.NoteID, &nt.Title, &nt.Cover, &nt.CreatorID, &nt.LikedNum, &nt.Portrait)
+	// 	if err != nil {
+	// 		fmt.Printf("scan failed, err:%v\n", err)
+	// 		return
+	// 	}
+	// 	notes = append(notes, nt)
+	// }
+	// return
+	var nt Collects
+	nt.NoteID = 10001
+	nt.Title = "l就别"
+	nt.Cover = "站发给"
+	nt.CreatorID = 10001
+	nt.LikedNum = 500
+	nt.Portrait = "/"
+	nt.CreatorName = "张菲"
+	notes = append(notes, nt)
+	return
+}
+
+func LikeInfoDB(id int) (notes []Likes) { // 从数据库获得用户信息
+	// sqlStr := `select n.noteId, n.title, n.cover,n.creatorAccount,n.likeNum, u.portrait
+	// from noteInfo n,userInfo u
+	// where n.creatorAccount = '"+id+"' and u.userAccount = '"+id+"'`
+	// rows, err := db.Query(sqlStr)
+	// if err != nil {
+	// 	fmt.Printf("query failed, err:%v\n", err)
+	// 	return
+	// }
+	// defer rows.Close()
+
+	// for rows.Next() {
+	// 	var nt Likes
+	// 	err := rows.Scan(&nt.NoteID, &nt.Title, &nt.Cover, &nt.CreatorID, &nt.LikedNum, &nt.Portrait)
+	// 	if err != nil {
+	// 		fmt.Printf("scan failed, err:%v\n", err)
+	// 		return
+	// 	}
+	// 	notes = append(notes, nt)
+	// }
+	// return
+	var nt Likes
+	nt.NoteID = 10001
+	nt.Title = "l就别"
+	nt.Cover = "站发给"
+	nt.CreatorID = 10001
+	nt.LikedNum = 500
+	nt.Portrait = "/"
+	nt.CreatorName = "张菲"
+	notes = append(notes, nt)
+	return
 }
 
 func CheckUser(userName, password string) bool {
--- web/webServer/routers/api/v1/users.go
+++ web/webServer/routers/api/v1/users.go
@@ -9,20 +9,20 @@ import (
 )
 
 type UserInfo struct {
-	Infos    models.UserInfo `json:"userInfo"` // 用户信息，只有一条，不用数组
-	Notes    []models.Note   `json:"notes"`    // 笔记，简要信息
-	Collects []models.Note   `json:"collects"`
-	Likes    []models.Note   `json:"likes"`
-	IsHost   bool            `json:"isHost"` //是否页面主人
+	Infos    []models.UserInfo `json:"userInfo"` // 用户信息，只有一条，不用数组
+	Notes    []models.Notes    `json:"notes"`    // 笔记，简要信息
+	Collects []models.Collects `json:"collects"`
+	Likes    []models.Likes    `json:"likes"`
+	IsHost   bool              `json:"isHost"` //是否页面主人
 }
 
 func GetUserInfo(c *gin.Context) { //显示用户界面全部信息
 	var info UserInfo
 	userID, _ := strconv.Atoi(c.Param("userID"))
 	info.Infos = models.UserInfoDB(userID) // 通过用户ID去数据库获取信息
-	info.Notes = models.GetBriefNtInfo()
-	info.Collects = models.GetBriefNtInfo()
-	info.Likes = models.GetBriefNtInfo()
+	info.Notes = models.NoteInfoDB(userID)
+	info.Collects = models.CollectInfoDB(userID)
+	info.Likes = models.LikeInfoDB(userID)
 	info.IsHost = true
 
 	c.JSON(http.StatusOK, gin.H{
